da5fc1d72f4249159e997ea2125dff6e48aa0981
Mark Westra <m.t.westra@akvo.org>
2014-09-25 14:52:13 +0200

    [#2] Use nodejs 0.10.0
    * nodejs 0.10.0 includes npm so we don't need to load it extra

--- setup/deb-deps.sh
+++ setup/deb-deps.sh
@@ -13,7 +13,7 @@ echo "================= getting repositories =================="
 add-apt-repository -y ppa:cartodb/base
 add-apt-repository -y ppa:cartodb/gis
 add-apt-repository -y ppa:mapnik/v2.1.0
-add-apt-repository -y ppa:cartodb/nodejs
+add-apt-repository -y ppa:cartodb/nodejs-010
 add-apt-repository -y ppa:cartodb/redis
 add-apt-repository -y ppa:cartodb/postgresql-9.3
 add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
@@ -25,7 +25,7 @@ echo "================= apt-get install =================="
 apt-get install -y make unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev libjson0 \
   python-simplejson libjson0-dev proj-bin proj-data libproj-dev postgresql-9.3 \
   postgresql-client-9.3 postgresql-contrib-9.3 postgresql-server-dev-9.3 \
-  postgresql-plpython-9.3 ruby1.9.1 ruby1.9.1-dev nodejs npm redis-server libmapnik-dev \
+  postgresql-plpython-9.3 ruby1.9.1 ruby1.9.1-dev nodejs redis-server libmapnik-dev \
   mapnik-utils python-mapnik git python-setuptools python-gdal gdal-bin libgdal1-dev \
   mercurial
 
